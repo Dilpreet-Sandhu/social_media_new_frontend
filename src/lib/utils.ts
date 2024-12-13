94c4a493ae3de79904089adab6dce173892c848c
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2024-12-13 15:01:08 +0530

    fixed local storage issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,8 +11,12 @@ export function cn(...inputs: ClassValue[]) {
 export function getOrSaveToLocalStorage({get,value,key} : {get?: boolean,value ?: any[],key : string}) {
 
 
+
   if (get) {
-    return JSON.parse(localStorage.getItem(key) || "");
+
+    const data = localStorage.getItem(key);
+
+    return data ? JSON.parse(data) : null;
   }
   else {
      localStorage.setItem(key,JSON.stringify(value));
